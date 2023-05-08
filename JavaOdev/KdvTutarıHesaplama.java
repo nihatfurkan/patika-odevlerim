94d4af6390196162c043d47a592d0f934bbf3ff7
nihat furkan adalar <nihatadalar@gmail.com>
2023-05-08 19:15:23 +0300

    düzeltme

--- JavaOdev/KdvTutarıHesaplama.java
+++ JavaOdev/KdvTutarıHesaplama.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class KdvTutarıHesaplama {
     public static void main (String[] args) {
 
-        double kdvOrani = 0.18, para, kdvTutari, kdvliFiyat;
+        double para, kdvTutari, kdvliFiyat;
 
         Scanner hesap= new Scanner(System.in);  //System.in ifadesi  kullanıcıdan veri girişi yapılacağını belirtiyor.
 
         System.out.print("Para Miktarını Giriniz : ");
         para = hesap.nextDouble(); // kullanıcının girdiği para değeri para değişkenine atanır.
 
+        double kdvOrani = (para > 1000) ? 0.08 : 0.18;
         kdvTutari = para * kdvOrani;
         kdvliFiyat = para + kdvTutari;
 
